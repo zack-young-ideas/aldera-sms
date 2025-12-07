0f6da041a67a279a9b661b22f477a319044f6835
Zack Young <zackyoung8709@gmail.com>
2025-12-07 18:24:28 -0500

    Fix import statement in SMS backends package
    The __init__.py file in the SMS backends package previously
    imported all of the backend modules to construct a dictionary
    of backend classes. However, the async_aws.py module relied on
    a third-party dependency called aioboto3 that is only installed
    if the user specifies the async option when they install aldera.
    Therefore, if the user installed aldera without the async option,
    and tried to import the SMS backends package, a
    ModuleNotFoundError was raised. This has been fixed.

--- src/aldera/sms/backends/__init__.py
+++ src/aldera/sms/backends/__init__.py
@@ -1,8 +1,13 @@
-from aldera.sms.backends import async_aws, aws, locmem
-
+from aldera.sms.backends import aws, locmem
 
 backend_classes = {
-    'async_aws': async_aws.AsyncSmsBackend,
     'aws': aws.SmsBackend,
     'locmem': locmem.SmsBackend,
 }
+
+try:
+    from aldera.sms.backends import async_aws
+except ModuleNotFoundError:
+    pass
+else:
+    backend_classes['async_aws'] = async_aws.AsyncSmsBackend
